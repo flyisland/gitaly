9e8dfb40fb56e2cdb6edb0189e18a24ffc17d095
Divya Rani <drani@gitlab.com>
2025-01-07 02:26:04 +0530

    raft: Implement gRPC transport interface
    This commit introduces a gRPC transport interface for handling Raft
    communication. It implements the logic for sending and receiving Raft
    messages, including packing and unpacking log data. Additionally, the
    RaftManagerRegistry facilitates Raft manager registration and partition
    lookup.
    For the address lookup, we introduce a static routing table for now. In
    future, the table can be dynamic so that the addresses are propagated
    through gossiping.

---
+++ internal/gitaly/storage/raftmgr/grpc_transport.go
@@ -0,0 +1,248 @@
+package raftmgr
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"gitlab.com/gitlab-org/gitaly/v16/internal/archive"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/config"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/mode"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/grpc/client"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/log"
+	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
+	"go.etcd.io/etcd/raft/v3/raftpb"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+)
+
+// Transport defines the interface for sending Raft protocol messages.
+type Transport interface {
+	// Send dispatches a batch of Raft messages. It returns an error if the sending fails. This function receives a
+	// context, the list of messages to send and a function that returns the path of WAL directory of a particular
+	// log entry. The implementation must respect input context's cancellation.
+	Send(ctx context.Context, walDirForLSN func(storage.LSN) string, partitionID uint64, messages []raftpb.Message) error
+	// Receive receives a Raft message and processes it.
+	Receive(ctx context.Context, authorityName string, partitionID uint64, raftMsg raftpb.Message) error
+}
+
+// GrpcTransport is a gRPC transport implementation for sending Raft messages across nodes.
+type GrpcTransport struct {
+	logger         log.Logger
+	cfg            config.Cfg
+	routingTable   RoutingTable
+	registry       ManagerRegistry
+	connectionPool *client.Pool
+}
+
+// NewGrpcTransport creates a new GrpcTransport instance.
+func NewGrpcTransport(logger log.Logger, cfg config.Cfg, routingTable RoutingTable, registry ManagerRegistry, conns *client.Pool) *GrpcTransport {
+	return &GrpcTransport{
+		logger:         logger,
+		cfg:            cfg,
+		routingTable:   routingTable,
+		registry:       registry,
+		connectionPool: conns,
+	}
+}
+
+// Send sends Raft messages to the appropriate nodes.
+func (t *GrpcTransport) Send(ctx context.Context, walDirForLSN func(storage.LSN) string, partitionID uint64, messages []raftpb.Message) error {
+	// Group by destination node
+	messagesByNode := make(map[uint64][]raftpb.Message)
+	for i := range messages {
+		nodeID := messages[i].To
+		messagesByNode[nodeID] = append(messagesByNode[nodeID], messages[i])
+	}
+
+	g := &errgroup.Group{}
+
+	for nodeID, msgs := range messagesByNode {
+		g.Go(func() error {
+			err := t.sendToNode(ctx, walDirForLSN, nodeID, partitionID, msgs)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+	}
+
+	return g.Wait()
+}
+
+func (t *GrpcTransport) sendToNode(ctx context.Context, walDirForLSN func(storage.LSN) string, nodeID uint64, partitionID uint64, msgs []raftpb.Message) error {
+	// For now, we are using a static routing table that contains mapping of nodeID to address. In future, the routing
+	// table can become dynamic so that storage addresses are propagated through gossiping.
+	addr, err := t.routingTable.Translate(nodeID)
+	if err != nil {
+		return fmt.Errorf("translate nodeID %d: %w", nodeID, err)
+	}
+
+	// Raft messages can contain entries intended for multiple storages. We need to get the storage name for the
+	// destination node.
+	storageName, err := t.routingTable.GetStorageName(nodeID)
+	if err != nil {
+		return fmt.Errorf("get storage name for nodeID %d: %w", nodeID, err)
+	}
+
+	// get the connection to the node
+	conn, err := t.connectionPool.Dial(ctx, addr, t.cfg.Auth.Token)
+	if err != nil {
+		return fmt.Errorf("get connection to node %d: %w", nodeID, err)
+	}
+
+	client := gitalypb.NewRaftServiceClient(conn)
+	stream, err := client.SendMessage(ctx)
+	if err != nil {
+		return fmt.Errorf("create stream to node %d: %w", nodeID, err)
+	}
+
+	for _, msg := range msgs {
+		for i := range msg.Entries {
+			if msg.Entries[i].Type != raftpb.EntryNormal {
+				continue
+			}
+
+			var raftMsg gitalypb.RaftEntry
+			if err := proto.Unmarshal(msg.Entries[i].Data, &raftMsg); err != nil {
+				return fmt.Errorf("unmarshalling entry type: %w", err)
+			}
+
+			// Pack the log data if needed
+			if raftMsg.GetData().GetPacked() == nil {
+				lsn := storage.LSN(msg.Entries[i].Index)
+				path := walDirForLSN(lsn)
+				if err := t.packLogData(ctx, lsn, &raftMsg, path); err != nil {
+					return fmt.Errorf("packing log data: %w", err)
+				}
+
+				// Marshal back into entry
+				data, err := proto.Marshal(&raftMsg)
+				if err != nil {
+					return fmt.Errorf("marshal entry: %w", err)
+				}
+				msg.Entries[i].Data = data
+			}
+		}
+
+		if err := stream.Send(&gitalypb.RaftMessageRequest{
+			ClusterId:     t.cfg.Raft.ClusterID,
+			AuthorityName: storageName,
+			PartitionId:   partitionID,
+			Message:       &msg,
+		}); err != nil {
+			return fmt.Errorf("send batch to node %d: %w", nodeID, err)
+		}
+	}
+
+	if _, err := stream.CloseAndRecv(); err != nil {
+		return fmt.Errorf("close stream to node %d: %w", nodeID, err)
+	}
+
+	return nil
+}
+
+func (t *GrpcTransport) packLogData(ctx context.Context, lsn storage.LSN, message *gitalypb.RaftEntry, logEntryPath string) error {
+	var logData bytes.Buffer
+	if err := archive.WriteTarball(ctx, t.logger.WithFields(log.Fields{
+		"raft.component":      "WAL archiver",
+		"raft.log_entry_lsn":  lsn,
+		"raft.log_entry_path": logEntryPath,
+	}), &logData, logEntryPath, "."); err != nil {
+		return fmt.Errorf("archiving WAL log entry: %w", err)
+	}
+	message.Data = &gitalypb.RaftEntry_LogData{
+		LocalPath: []byte(logEntryPath),
+		Packed:    logData.Bytes(),
+	}
+	return nil
+}
+
+// Receive receives a stream of Raft messages and processes them.
+func (t *GrpcTransport) Receive(ctx context.Context, authorityName string, partitionID uint64, raftMsg raftpb.Message) error {
+	// Retrieve the raft manager from the registry, assumption is that all the messages are from the same partition key.
+	raftManager, err := t.registry.GetManager(PartitionKey{
+		authorityName: authorityName,
+		partitionID:   partitionID,
+	})
+	if err != nil {
+		return status.Errorf(codes.NotFound, "raft manager not found for partition %d: %v",
+			partitionID, err)
+	}
+
+	for _, entry := range raftMsg.Entries {
+		var msg gitalypb.RaftEntry
+		if err := proto.Unmarshal(entry.Data, &msg); err != nil {
+			return status.Errorf(codes.InvalidArgument, "failed to unmarshal message: %v", err)
+		}
+
+		if msg.GetData().GetPacked() != nil {
+			if err := unpackLogData(&msg, raftManager.GetEntryPath(storage.LSN(entry.Index))); err != nil {
+				return status.Errorf(codes.Internal, "failed to unpack log data: %v", err)
+			}
+		}
+	}
+
+	// Step messages per partition with their respective entries
+	if err := raftManager.Step(ctx, raftMsg); err != nil {
+		return status.Errorf(codes.Internal, "failed to step message: %v", err)
+	}
+
+	return nil
+}
+
+func unpackLogData(msg *gitalypb.RaftEntry, logEntryPath string) error {
+	logData := msg.GetData().GetPacked()
+
+	if err := os.MkdirAll(filepath.Dir(logEntryPath), mode.Directory); err != nil {
+		return fmt.Errorf("creating WAL directory: %w", err)
+	}
+
+	tarReader := tar.NewReader(bytes.NewReader(logData))
+	for {
+		header, err := tarReader.Next()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		actualName := header.Name
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			// create the directory if not exists
+			if _, err := os.Stat(filepath.Join(logEntryPath, actualName)); os.IsNotExist(err) {
+				if err := os.Mkdir(filepath.Join(logEntryPath, actualName), mode.Directory); err != nil {
+					return fmt.Errorf("creating directory: %w", err)
+				}
+			}
+		case tar.TypeReg:
+			if err := func() error {
+				path := filepath.Join(logEntryPath, actualName)
+				f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, mode.File)
+				if err != nil {
+					return fmt.Errorf("writing log entry file: %w", err)
+				}
+				defer f.Close()
+
+				if _, err := io.Copy(f, tarReader); err != nil {
+					return fmt.Errorf("writing log entry file: %w", err)
+				}
+
+				return nil
+			}(); err != nil {
+				return err
+			}
+
+		}
+	}
+
+	return nil
+}
---
+++ internal/gitaly/storage/raftmgr/manager_registry.go
@@ -0,0 +1,50 @@
+package raftmgr
+
+import (
+	"fmt"
+	"sync"
+)
+
+// PartitionKey is used to uniquely identify a partition.
+type PartitionKey struct {
+	authorityName string
+	partitionID   uint64
+}
+
+// ManagerRegistry is an interface that defines the methods to register and retrieve managers.
+type ManagerRegistry interface {
+	// GetManager returns the manager for a given partition key.
+	GetManager(key PartitionKey) (RaftManager, error)
+	// RegisterManager registers a manager for a given partition key.
+	RegisterManager(key PartitionKey, manager RaftManager) error
+}
+
+// RaftManagerRegistry is a concrete implementation of the ManagerRegistry interface.
+type raftManagerRegistry struct {
+	managers *sync.Map
+}
+
+// NewRaftManagerRegistry creates a new RaftManagerRegistry.
+func NewRaftManagerRegistry() *raftManagerRegistry {
+	return &raftManagerRegistry{managers: &sync.Map{}}
+}
+
+// GetManager returns the manager for a given partitionKey.
+func (r *raftManagerRegistry) GetManager(key PartitionKey) (RaftManager, error) {
+	r.managers.Range(func(k, v any) bool {
+		fmt.Printf("key %+v value %+v\n", k, v)
+		return true
+	})
+	if mgr, ok := r.managers.Load(key); ok {
+		return mgr.(RaftManager), nil
+	}
+	return nil, fmt.Errorf("no manager found for partition key %+v", key)
+}
+
+// RegisterManager registers a manager for a given partitionKey.
+func (r *raftManagerRegistry) RegisterManager(key PartitionKey, manager RaftManager) error {
+	if _, loaded := r.managers.LoadOrStore(key, manager); loaded {
+		return fmt.Errorf("manager already registered for partition key %+v", key)
+	}
+	return nil
+}
