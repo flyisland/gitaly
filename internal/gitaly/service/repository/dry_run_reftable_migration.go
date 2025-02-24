6e4608165e1cffd35285c8c95c46fb475ddf47d8
Karthik Nayak <knayak@gitlab.com>
2025-02-24 12:31:46 +0100

    repository: Add RPC to dry-run the reftable migrations
    To dry-run the reftable migration, we cannot use the migration
    frameworks's dry-run functionality since the goroutines is bound to the
    partition and the partition is closed when RPCs which triggered the
    partition complete.
    
    This means, long-running migrations are cancelled before completion. To
    get around this, we want to introduce an RPC who's purpose is to run the
    dry-run the reftable migration. The RPC will capture the metrics needed
    and finally return an error to ensure that the transaction manager
    rollsback the transaction.

---
+++ internal/gitaly/service/repository/dry_run_reftable_migration.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"context"
+	"time"
+
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage/storagemgr/partition/migration"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/log"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/structerr"
+	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
+	"google.golang.org/protobuf/types/known/durationpb"
+)
+
+func (s *server) DryRunReftableMigration(
+	ctx context.Context,
+	in *gitalypb.DryRunReftableMigrationRequest,
+) (*gitalypb.DryRunReftableMigrationResponse, error) {
+	if err := s.locator.ValidateRepository(ctx, in.GetRepository()); err != nil {
+		return nil, structerr.NewInvalidArgument("%w", err)
+	}
+
+	migrator := migration.NewReftableMigration(1, s.localRepoFactory)
+	if migrator.IsDisabled(ctx) {
+		return nil, structerr.NewFailedPrecondition("migration disabled")
+	}
+
+	tx := storage.ExtractTransaction(ctx)
+	if tx == nil {
+		return nil, structerr.NewInternal("transaction not found")
+	}
+
+	logger := log.FromContext(ctx)
+	t := time.Now()
+
+	if err := migrator.Fn(ctx,
+		tx,
+		in.GetRepository().GetStorageName(),
+		tx.OriginalRepository(in.GetRepository()).GetRelativePath(),
+	); err != nil {
+		return nil, structerr.NewInternal("migration failed: %w", err)
+	}
+
+	duration := time.Since(t)
+	resp := &gitalypb.DryRunReftableMigrationResponse{
+		Time: durationpb.New(duration),
+	}
+
+	logger.WithField("migration_time", duration).Info("migration ran successfully")
+
+	// Return an error on purpose so that the transaction is rolled back.
+	return resp, structerr.NewInternal("error to rollback transaction")
+}
---
+++ internal/gitaly/service/repository/dry_run_reftable_migration_test.go
@@ -0,0 +1,100 @@
+package repository
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/featureflag"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git/gittest"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git/localrepo"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/structerr"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/testhelper"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/testhelper/testserver"
+	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
+)
+
+func TestDryRunReftableMigration(t *testing.T) {
+	t.Parallel()
+
+	testhelper.NewFeatureSets(
+		featureflag.ReftableMigration,
+	).Run(t, testDryRunReftableMigration)
+}
+
+func testDryRunReftableMigration(t *testing.T, ctx context.Context) {
+	t.Parallel()
+
+	logger := testhelper.NewLogger(t)
+	hook := testhelper.AddLoggerHook(logger)
+
+	cfg, client := setupRepositoryService(t, testserver.WithLogger(logger))
+
+	repoProto, repoPath := gittest.CreateRepository(t, ctx, cfg, gittest.CreateRepositoryConfig{})
+	gittest.WriteCommit(t, cfg, repoPath, gittest.WithBranch("master"))
+
+	for _, tc := range []struct {
+		desc            string
+		req             *gitalypb.DryRunReftableMigrationRequest
+		expectedErr     error
+		expectedSuccess bool
+	}{
+		{
+			desc: "repository nil",
+			req: &gitalypb.DryRunReftableMigrationRequest{
+				Repository: nil,
+			},
+			expectedErr:     structerr.NewInvalidArgument("%w", storage.ErrRepositoryNotSet),
+			expectedSuccess: false,
+		},
+		{
+			desc: "repository exists",
+			req: &gitalypb.DryRunReftableMigrationRequest{
+				Repository: &gitalypb.Repository{
+					StorageName:  repoProto.GetStorageName(),
+					RelativePath: repoProto.GetRelativePath(),
+				},
+			},
+			expectedErr: testhelper.EnabledOrDisabledFlag(ctx,
+				featureflag.ReftableMigration,
+				testhelper.WithOrWithoutWAL(
+					structerr.NewInternal("error to rollback transaction"),
+					structerr.NewInternal("transaction not found"),
+				),
+				structerr.NewFailedPrecondition("migration disabled"),
+			),
+			expectedSuccess: testhelper.EnabledOrDisabledFlag(ctx,
+				featureflag.ReftableMigration,
+				testhelper.WithOrWithoutWAL(true, false),
+				false,
+			),
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			before, err := localrepo.NewTestRepo(t, cfg, repoProto).ReferenceBackend(ctx)
+			require.NoError(t, err)
+
+			resp, err := client.DryRunReftableMigration(ctx, tc.req)
+			testhelper.RequireGrpcError(t, tc.expectedErr, err)
+
+			after, err := localrepo.NewTestRepo(t, cfg, repoProto).ReferenceBackend(ctx)
+			require.NoError(t, err)
+
+			// Ensure that the dry-run doesn't actually modify the repository
+			require.Equal(t, before, after)
+
+			if tc.expectedSuccess {
+				for _, entry := range hook.AllEntries() {
+					if entry.Message == "migration ran successfully" {
+						return
+					}
+				}
+				t.Error("migration success log missing")
+				require.True(t, resp.GetTime().IsValid())
+			} else {
+				require.False(t, resp.GetTime().IsValid())
+			}
+		})
+	}
+}
