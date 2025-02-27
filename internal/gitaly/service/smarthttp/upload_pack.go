b45b7fae7c78aa0c7bcf83a678d35d96d111a2cf
Olivier Campeau <ocampeau@gitlab.com>
2025-02-27 14:15:56 -0500

    Bundle-Uri: Activate bundle generation
    While everything is in place to support generating
    bundles, it is missing the actual call to the
    function that will generate those bundles. This
    omission was thoughtfully planned because by
    enabling it into an isolated commit, it makes it
    easiser to revert in case we face some issues.
    
    This commit introduces the call to the function that
    will generate bundles, effectively enabling bundle
    generation (when the appropriate feature flags are on).
    
    References:
    https://gitlab.com/gitlab-org/gitaly/-/issues/5656

--- internal/gitaly/service/smarthttp/upload_pack.go
+++ internal/gitaly/service/smarthttp/upload_pack.go
@@ -117,6 +117,14 @@ func (s *server) runUploadPack(ctx context.Context, req *gitalypb.PostUploadPack
 
 	repo := s.localRepoFactory.Build(req.GetRepository())
 	if s.bundleURIManager != nil {
+		// Bundle generation is an optimization that is transparent to users.
+		// If it fails, we log the error but continue with the regular upload-pack
+		// operation without the bundle optimization.
+		// If successful, a goroutine is spawned to generate the bundle, in which case
+		// the bundle generation becomes independent of the RPC request.
+		if err := s.bundleURIManager.GenerateWithStrategy(ctx, repo); err != nil {
+			s.logger.WithError(err).Error("failed generating bundle URI manager")
+		}
 		gitConfig = append(gitConfig, s.bundleURIManager.UploadPackGitConfig(ctx, req.GetRepository())...)
 	} else {
 		gitConfig = append(gitConfig, bundleuri.CapabilitiesGitConfig(ctx, false)...)
