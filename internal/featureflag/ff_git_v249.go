d6cd5cfe39f221bcfdbc4576f1fe13f9f77431b8
Justin Tobler <jtobler@gitlab.com>
2025-04-22 10:15:23 -0500

    featureflag: Remove Git version 2.49 flag
    The Git version 2.49 feature flag was default enabled in the GitLab
    17.11 release. Remove the feature flag.

--- internal/featureflag/ff_git_v249.go
+++
@@ -1,9 +0,0 @@
-package featureflag
-
-// GitV249 enables the use Git v2.49.
-var GitV249 = NewFeatureFlag(
-	"git_v249",
-	"v17.11.0",
-	"https://gitlab.com/gitlab-org/gitaly/-/issues/6690",
-	true,
-)
--- internal/git/gitcmd/execution_environment.go
+++ internal/git/gitcmd/execution_environment.go
@@ -23,9 +23,6 @@ var (
 	BundledGitConstructors = []BundledGitEnvironmentConstructor{
 		{
 			Suffix: "-v2.49",
-			FeatureFlags: []featureflag.FeatureFlag{
-				featureflag.GitV249,
-			},
 		},
 		{
 			Suffix: "-v2.48",
