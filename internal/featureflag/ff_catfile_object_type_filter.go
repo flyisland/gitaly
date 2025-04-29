3c6ded8148a3574233c902526e0e7d251364d808
Patrick Steinhardt <psteinhardt@gitlab.com>
2025-04-29 08:53:38 +0200

    commit: Use object type filters in `ListAllCommits()`
    Convert `ListAllCommits()` to use object type filters as introduced in
    a preceding commit. This should significantly speed up listing all
    commits in cases where we have packfile bitmaps.

---
+++ internal/featureflag/ff_catfile_object_type_filter.go
@@ -0,0 +1,10 @@
+package featureflag
+
+// CatfileObjectTypeFilter enables git-cat-file(1) to filter objects via the new `--filter=object:type` option. This
+// should lead to a significant speedup for certain RPCs.
+var CatfileObjectTypeFilter = NewFeatureFlag(
+	"catfile_object_type_filter",
+	"v18.0.0",
+	"https://gitlab.com/gitlab-org/gitaly/-/issues/6720",
+	false,
+)
