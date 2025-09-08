c9259604cbcac13ee43be704dc76ea99d66b027d
James Liu <jliu@gitlab.com>
2025-09-08 15:00:00 +1000

    benchmark: Define testdata in repositories config
    The k6-benchmark.js script currently hardcodes OIDs, references, and
    paths for use in gRPC requests. The values are only applicable to a
    single repository (the `git` source) and are static across the lifetime
    of the test.
    
    In order to support multiple repositories, we should remove the
    hardcoded test inputs and allow them to be defined alongside the
    repositories in the config.yml file. The RPCs we're currently calling
    require four types of inputs:
    
    - commitish OIDs (i.e. commits)
    - references
    - filenames
    - directories
    
    Modify the config.yml.example to include a few examples of each type.
    Modify the Ansible script to convert the `repositories` block into JSON
    and write it to the client machine. A subsequent commit will modify the
    k6-benchmark.js script to consume the config file instead of hardcoding
    the values.

--- _support/benchmarking/terraform/locals.tf
+++ _support/benchmarking/terraform/locals.tf
@@ -1,3 +1,4 @@
 locals {
   config = try(yamldecode(file("../${path.root}/config.yml")), yamldecode(file("../${path.root}/config.yml.example")))
+  repository_urls = [for repo in local.config.repositories : repo.remote]
 }
