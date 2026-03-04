92966130aa08aa4643d097053202d02bc38bc0bf
Toon Claes <toon@gitlab.com>
2026-03-04 13:45:01 +0100

    featureflag: Remove default-enabled git_last_modified
    The implementation of using git-last-modified(1) deemed to be stable.
    Remove the feature flag and the old implementation.

--- internal/featureflag/ff_git_last_modified.go
+++
@@ -1,9 +0,0 @@
-package featureflag
-
-// GitLastModified enables the use of native git-last-modified(1)
-var GitLastModified = NewFeatureFlag(
-	"git_last_modified",
-	"v18.5",
-	"https://gitlab.com/gitlab-org/gitaly/-/issues/6911",
-	true,
-)
