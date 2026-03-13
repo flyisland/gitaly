d7e6bee4cf1d5b578a4ab8925319e699266dbec3
James Liu <jliu@gitlab.com>
2026-03-13 16:36:36 +1100

    diff: Extract git-diff --numstat into a helper function
    The DiffStats RPC executes git-diff --numstat to compute the number of
    added and deleted lines between two revisions. A subsequent commit will
    need to reuse this functionality in the context of a different RPC.
    
    Extract the bulk of the handler into a helper function that accepts a
    callback. Different RPC handlers can provide different callbacks.

--- internal/gitaly/service/diff/numstat.go
+++ internal/gitaly/service/diff/numstat.go
@@ -1,10 +1,12 @@
 package diff
 
 import (
+	"context"
 	"errors"
 	"io"
 
 	"gitlab.com/gitlab-org/gitaly/v18/internal/git/gitcmd"
+	"gitlab.com/gitlab-org/gitaly/v18/internal/git/localrepo"
 	"gitlab.com/gitlab-org/gitaly/v18/internal/gitaly/diff"
 	"gitlab.com/gitlab-org/gitaly/v18/internal/structerr"
 	"gitlab.com/gitlab-org/gitaly/v18/proto/go/gitalypb"
@@ -20,10 +22,48 @@ func (s *server) DiffStats(in *gitalypb.DiffStatsRequest, stream gitalypb.DiffSe
 	repo := s.localRepoFactory.Build(in.GetRepository())
 
 	var batch []*gitalypb.DiffStats
-	cmd, err := repo.Exec(stream.Context(), gitcmd.Command{
-		Name:  "diff",
-		Flags: []gitcmd.Option{gitcmd.Flag{Name: "--numstat"}, gitcmd.Flag{Name: "-z"}},
-		Args:  []string{in.GetLeftCommitId(), in.GetRightCommitId()},
+	if err := numstat(stream.Context(), repo, in.GetLeftCommitId(), in.GetRightCommitId(), nil, func(stat diff.NumStat) error {
+		batch = append(batch, &gitalypb.DiffStats{
+			Additions: stat.Additions,
+			Deletions: stat.Deletions,
+			Path:      stat.Path,
+			OldPath:   stat.OldPath,
+		})
+
+		if len(batch) == maxNumStatBatchSize {
+			if err := sendStats(batch, stream); err != nil {
+				return err
+			}
+
+			batch = nil
+		}
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return sendStats(batch, stream)
+}
+
+func sendStats(batch []*gitalypb.DiffStats, stream gitalypb.DiffService_DiffStatsServer) error {
+	if len(batch) == 0 {
+		return nil
+	}
+
+	if err := stream.Send(&gitalypb.DiffStatsResponse{Stats: batch}); err != nil {
+		return structerr.NewInternal("send: %w", err)
+	}
+
+	return nil
+}
+
+func numstat(ctx context.Context, repo *localrepo.Repo, leftCommitID, rightCommitID string, paths []string, cb func(diff.NumStat) error) error {
+	cmd, err := repo.Exec(ctx, gitcmd.Command{
+		Name:        "diff",
+		Flags:       []gitcmd.Option{gitcmd.Flag{Name: "--numstat"}, gitcmd.Flag{Name: "-z"}},
+		Args:        []string{leftCommitID, rightCommitID},
+		PostSepArgs: paths,
 	}, gitcmd.WithSetupStdout())
 	if err != nil {
 		return structerr.NewInternal("cmd: %w", err)
@@ -41,38 +81,13 @@ func (s *server) DiffStats(in *gitalypb.DiffStatsRequest, stream gitalypb.DiffSe
 			return err
 		}
 
-		numStat := &gitalypb.DiffStats{
-			Additions: stat.Additions,
-			Deletions: stat.Deletions,
-			Path:      stat.Path,
-			OldPath:   stat.OldPath,
-		}
-
-		batch = append(batch, numStat)
-
-		if len(batch) == maxNumStatBatchSize {
-			if err := sendStats(batch, stream); err != nil {
-				return err
-			}
-
-			batch = nil
+		if err := cb(*stat); err != nil {
+			return err
 		}
 	}
 
 	if err := cmd.Wait(); err != nil {
 		return structerr.NewFailedPrecondition("%w", err)
-	}
-
-	return sendStats(batch, stream)
-}
-
-func sendStats(batch []*gitalypb.DiffStats, stream gitalypb.DiffService_DiffStatsServer) error {
-	if len(batch) == 0 {
-		return nil
-	}
-
-	if err := stream.Send(&gitalypb.DiffStatsResponse{Stats: batch}); err != nil {
-		return structerr.NewInternal("send: %w", err)
 	}
 
 	return nil
