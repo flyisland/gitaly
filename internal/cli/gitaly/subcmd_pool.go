0e3e5f62285144a813a7d03bf15828f01e684887
James Liu <jliu@gitlab.com>
2026-03-16 14:14:32 +1100

    pools: Remove connection to GitLab DB
    The gitlab-db-dsn argument was added to the pools subcommand as a means
    of connecting to the main GitLab Postgres database. This was intended to
    be used for debugging purposes. A subsequent commit will query a new
    Object Pool Members API in Rails to retrieve the same information.
    
    Remove the argument and associated code that queries the database
    directly.

--- internal/cli/gitaly/subcmd_pool.go
+++ internal/cli/gitaly/subcmd_pool.go
@@ -2,12 +2,9 @@ package gitaly
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"io"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/urfave/cli/v3"
 	"gitlab.com/gitlab-org/gitaly/v18/internal/cli/common"
@@ -42,10 +39,6 @@ Example:
 				Usage:    "path to the gitaly config file",
 				Required: true,
 			},
-			&cli.StringFlag{
-				Name:  "gitlab-db-dsn",
-				Usage: "PostgreSQL DSN for the GitLab database to query upstream repositories (optional)",
-			},
 		},
 		Before: func(ctx context.Context, cmd *cli.Command) (context.Context, error) {
 			if cmd.Args().Present() {
@@ -61,7 +54,6 @@ func poolAction(ctx context.Context, cmd *cli.Command) error {
 	logger := log.ConfigureCommand()
 
 	gitalyConfigPath := cmd.String("config")
-	gitlabDBDSN := cmd.String("gitlab-db-dsn")
 
 	file, err := os.Open(gitalyConfigPath)
 	if err != nil {
@@ -96,25 +88,6 @@ func poolAction(ctx context.Context, cmd *cli.Command) error {
 		gitalyStorages: cfg.Storages,
 	}
 
-	// TODO: Remove this GitLab DB connection logic once we have a way to set upstream info via Rails API when creating pools.
-	if gitlabDBDSN != "" {
-		logger.Debug("connecting to GitLab database...")
-		gitlabDB, err := sql.Open("pgx", gitlabDBDSN)
-		if err != nil {
-			return fmt.Errorf("open gitlab database: %w", err)
-		}
-		defer func() { _ = gitlabDB.Close() }()
-
-		pingCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
-		defer cancel()
-		if err := gitlabDB.PingContext(pingCtx); err != nil {
-			return fmt.Errorf("ping gitlab database: %w", err)
-		}
-		scanner.gitlabDB = gitlabDB
-		logger.Debug("connected to GitLab database")
-		fmt.Fprintln(cmd.Writer, "gitlab db: connected")
-	}
-
 	if err := scanner.scanStorages(ctx); err != nil {
 		return fmt.Errorf("scan storages: %w", err)
 	}
@@ -128,7 +101,6 @@ type poolScanner struct {
 	out            io.Writer
 	internalClient gitalypb.InternalGitalyClient
 	gitalyStorages []gitalycfg.Storage
-	gitlabDB       *sql.DB
 }
 
 func (ps *poolScanner) scanStorages(ctx context.Context) error {
@@ -166,87 +138,5 @@ func (ps *poolScanner) scanStorage(ctx context.Context, gitalyStorage gitalycfg.
 		}
 	}
 
-	if ps.gitlabDB != nil {
-		if err := ps.enrichWithUpstream(ctx, gitalyStorage.Name); err != nil {
-			ps.logger.WithError(err).Warn("failed to enrich with upstream")
-		}
-	} else {
-		fmt.Fprintln(ps.out, "gitlab db: not connected, skipping upstream enrichment")
-	}
-
 	return nil
-}
-
-func (ps *poolScanner) enrichWithUpstream(ctx context.Context, storageName string) error {
-	pools, err := common.ListPoolMetadata(ctx, ps.internalClient, storageName)
-	if err != nil {
-		return fmt.Errorf("list pools: %w", err)
-	}
-
-	fmt.Fprintf(ps.out, "enriching %d pools with upstream info\n", len(pools))
-
-	var upstreamMembers []common.PoolMember
-	for _, poolDiskPath := range pools {
-
-		fmt.Fprintf(ps.out, "upstream lookup: pool=%s\n", poolDiskPath)
-		upstream, err := ps.getUpstreamFromGitLab(ctx, poolDiskPath)
-		if err != nil {
-			ps.logger.WithError(err).WithField("pool", poolDiskPath).Debug("failed to get upstream from GitLab")
-			fmt.Fprintf(ps.out, "upstream lookup failed: %v\n", err)
-			continue
-		}
-
-		fmt.Fprintf(ps.out, "upstream found: %s\n", upstream)
-
-		upstreamMembers = append(upstreamMembers, common.PoolMember{
-			MemberDiskPath: upstream,
-			PoolDiskPath:   poolDiskPath,
-			IsUpstream:     true,
-		})
-	}
-
-	if len(upstreamMembers) > 0 {
-		if err := common.StorePoolMetadata(ctx, ps.internalClient, storageName, upstreamMembers); err != nil {
-			return fmt.Errorf("store upstream metadata: %w", err)
-		}
-		fmt.Fprintf(ps.out, "upstream stored: %d members added\n", len(upstreamMembers))
-	}
-
-	return nil
-}
-
-func (ps *poolScanner) getUpstreamFromGitLab(ctx context.Context, poolDiskPath string) (string, error) {
-	if ps.gitlabDB == nil {
-		return "", fmt.Errorf("GitLab database not connected")
-	}
-
-	var sourceProjectID int64
-	err := ps.gitlabDB.QueryRowContext(ctx,
-		`SELECT source_project_id FROM pool_repositories WHERE disk_path = $1 LIMIT 1`,
-		strings.TrimSuffix(poolDiskPath, ".git"),
-	).Scan(&sourceProjectID)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return "", fmt.Errorf("pool not found in pool_repositories: %s", poolDiskPath)
-		}
-		return "", fmt.Errorf("query pool_repositories: %w", err)
-	}
-
-	var upstreamDiskPath string
-	err = ps.gitlabDB.QueryRowContext(ctx,
-		`SELECT disk_path FROM project_repositories WHERE project_id = $1 LIMIT 1`,
-		sourceProjectID,
-	).Scan(&upstreamDiskPath)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return "", fmt.Errorf("source project not found: %d", sourceProjectID)
-		}
-		return "", fmt.Errorf("query project_repositories: %w", err)
-	}
-
-	if !strings.HasSuffix(upstreamDiskPath, ".git") {
-		upstreamDiskPath = upstreamDiskPath + ".git"
-	}
-
-	return upstreamDiskPath, nil
 }
