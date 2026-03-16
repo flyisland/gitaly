13aa3cda4fa0be4f1a2fb0aa74dd6399fa998d28
James Liu <jliu@gitlab.com>
2026-03-16 15:55:30 +1100

    deps: Thread gitlab.Client through the server dependencies

--- internal/gitaly/service/internalgitaly/server.go
+++ internal/gitaly/service/internalgitaly/server.go
@@ -5,24 +5,27 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v18/internal/gitaly/service"
 	"gitlab.com/gitlab-org/gitaly/v18/internal/gitaly/storage"
 	"gitlab.com/gitlab-org/gitaly/v18/internal/gitaly/storage/relational"
+	"gitlab.com/gitlab-org/gitaly/v18/internal/gitlab"
 	"gitlab.com/gitlab-org/gitaly/v18/internal/log"
 	"gitlab.com/gitlab-org/gitaly/v18/proto/go/gitalypb"
 )
 
 type server struct {
 	gitalypb.UnimplementedInternalGitalyServer
-	logger    log.Logger
-	storages  []config.Storage
-	locator   storage.Locator
-	poolStore relational.PoolStore
+	logger       log.Logger
+	storages     []config.Storage
+	locator      storage.Locator
+	poolStore    relational.PoolStore
+	gitlabClient gitlab.Client
 }
 
 // NewServer return an instance of the Gitaly service.
 func NewServer(deps *service.Dependencies) gitalypb.InternalGitalyServer {
 	return &server{
-		logger:    deps.GetLogger(),
-		storages:  deps.GetCfg().Storages,
-		locator:   deps.GetLocator(),
-		poolStore: deps.GetPoolMetadataStore(),
+		logger:       deps.GetLogger(),
+		storages:     deps.GetCfg().Storages,
+		locator:      deps.GetLocator(),
+		poolStore:    deps.GetPoolMetadataStore(),
+		gitlabClient: deps.GetGitlabClient(),
 	}
 }
