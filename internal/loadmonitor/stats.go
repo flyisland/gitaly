7d44f17e152be132a6a974c2e59434c53532523f
Olivier Campeau <ocampeau@gitlab.com>
2026-04-16 13:19:12 -0400

    loadMonitor: Add more information to an Event
    This commit adds a new Name field to the load monitor Event to
    track which Condition triggered a particular Event. The Description
    field is repurposed to capture the value emitted from a Condition
    which holds the description of the event that triggered a Condition.
    
    The previous Stats are also added to the Event.
    
    Finally, a timestamp is associated with a Stats to track the
    poll time of this particular instance.

--- internal/loadmonitor/stats.go
+++ internal/loadmonitor/stats.go
@@ -1,8 +1,18 @@
 package loadmonitor
 
-import "gitlab.com/gitlab-org/gitaly/v18/internal/cgroups"
+import (
+	"time"
+
+	"gitlab.com/gitlab-org/gitaly/v18/internal/cgroups"
+)
 
 // Stats wraps statistics from different sources.
 type Stats struct {
-	CGroup cgroups.Stats
+	pollTime time.Time
+	CGroup   cgroups.Stats
+}
+
+// PollTime returns the poll time of the current Stats instance
+func (s Stats) PollTime() time.Time {
+	return s.pollTime
 }
